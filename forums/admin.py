c845586aa58fa96ce8a065b08981d3bac17402d1
outlier96 <awodeabubakar@gmail.com>
2021-07-04 23:03:00 +0100

    first commit

---
+++ forums/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import *
+ 
+#my models
+admin.site.register(forum)
+admin.site.register(Discussion)
---
+++ forums/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class forum(models.Model):
+    name=models.CharField(max_length=200,default="anonymous" )
+    email=models.CharField(max_length=200,null=True)
+    topic= models.CharField(max_length=300)
+    description = models.CharField(max_length=1000,blank=True)
+    link = models.CharField(max_length=100 ,null =True)
+    date_created=models.DateTimeField(auto_now_add=True,null=True)
+    
+    def __str__(self):
+        return str(self.topic)
+
+class categories(models.Model):
+     choose_category=[('ff','fish farming'),('af','animal farming'),('cr','crops'),('bp','best_practices'),('gn','general')]       
+ 
+class Discussion(models.Model):
+    forum = models.ForeignKey(forum,blank=True,on_delete=models.CASCADE)
+    discuss = models.CharField(max_length=1000)
+ 
+    def __str__(self):
+        return str(self.forum)
+
+       
+ 
